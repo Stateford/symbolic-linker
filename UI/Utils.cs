e59b4ccdc078eceaab466300bd187232bb15a572
Dimitri <idietmoran@gmail.com>
2019-01-20 13:12:24 -0500

    added unit tests

---
+++ UI/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UI
+{
+    public class Utils
+    {
+        public static bool VerifyPath(string path)
+        {
+            try
+            {
+                Path.GetFullPath(path);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return Path.IsPathRooted(path);
+        }
+    }
+}
---
+++ UnitTest/TestUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UI;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestUtils
+    {
+        [TestMethod]
+        public void TestVerifyPath()
+        {
+            var dir = Directory.GetCurrentDirectory();
+            Assert.IsTrue(Utils.VerifyPath(dir));
+        }
+    }
+}
